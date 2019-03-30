34c6e4d67549814612bf502d97b12b7dfc6f7d62
Tristan <tristan.morisot@viacesi.fr>
2019-03-30 10:15:51 +0100

    Close the app when all window are closed

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
- * Copyright © 2/21/19 4:06 PM, $name
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders X be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
- *
- * Except as contained in this notice, the name of the <copyright holders> shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written authorization from the $name.
- */
-
 const electron = require('electron');
 const { app, BrowserWindow, ipcMain, Notification } = electron;
 const path = require('path');
@@ -58,7 +47,7 @@ app.on('ready', () => {
     if (end) {
       new Notification({
         title: `Take a ${params.max === params.current ? 'long':'short'} break`,
-        body: `Pomodoro phase ${params.current} done, ${params.max === params.current ? '25':'5'} minutes of free time`,
+        body: `Pomodoro phase ${params.current - 1} done, ${params.max === params.current ? '25':'5'} minutes of free time`,
       }).show();
     }
     window.focus();
@@ -81,4 +70,7 @@ app.on('ready', () => {
     }
   });
 
+  app.on('window-all-closed', () => {
+    app.quit();
+  })
 });
