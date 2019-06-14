48f9786244b671f2b832e3ec7110d74e010b66ee
Tristan <llaunch14@gmail.com>
2019-06-14 22:16:11 +0200

    Adapt code to darwin os

--- index.js
+++ index.js
@@ -1,109 +1,121 @@
-const electron = require('electron');
-const { app, BrowserWindow, ipcMain, Notification, systemPreferences } = electron;
-const path = require('path');
+const electron = require("electron");
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Notification,
+  systemPreferences
+} = electron;
+const path = require("path");
+const os = require("os");
 
-app.on('ready', () => {
-
-  if (process.argv.includes('dev')) {
-    require('vue-devtools').install();
-    require('devtron').install();
+app.on("ready", () => {
+  if (process.argv.includes("dev")) {
+    require("vue-devtools").install();
+    require("devtron").install();
   }
 
   const xPos = electron.screen.getPrimaryDisplay().workArea.width;
   let window = new BrowserWindow({
     width: 300,
     height: 201,
-    transparent: true,
-    vibrancy: 'appearance-based',
+    transparent: os.platform() === 'darwin',
+    vibrancy: "appearance-based",
     x: xPos - 50 - 300,
     y: 65,
-    title: 'test',
+    title: "test",
     frame: false,
-    titleBarStyle: 'hidden',
+    titleBarStyle: "hidden",
     alwaysOnTop: true,
     maximizable: false,
-    resizable: process.argv.includes('dev'),
+    resizable: process.argv.includes("dev"),
     fullscreenable: false,
     fullscreenWindowTitle: true,
     movable: true,
-    icon: './public/assets/img/icon.png'
+    icon: "./public/assets/img/icon.png"
   });
 
-  window.setVisibleOnAllWorkspaces(true, {visibleOnFullScreen: true});
+  window.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
 
-  window.loadFile(path.resolve(__dirname, './public/final/index.html'));
+  window.loadFile(path.resolve(__dirname, "/public/final/index.html"));
 
-  window.webContents.executeJavaScript('window.localStorage.getItem(\'alwaysOnTop\')')
+  window.webContents
+    .executeJavaScript("window.localStorage.getItem('alwaysOnTop')")
     .then(result => {
-      window.setAlwaysOnTop(result === 'true');
+      window.setAlwaysOnTop(result === "true");
     })
     .catch(err => console.error(err));
 
-  systemPreferences.subscribeNotification(
-    'AppleInterfaceThemeChangedNotification',
-    function theThemeHasChanged () {
-      window.webContents.send('darkmode', systemPreferences.isDarkMode())
-    }
-  );
+  if (os.platform() === "darwin") {
+    systemPreferences.subscribeNotification(
+        'AppleInterfaceThemeChangedNotification',
+        function theThemeHasChanged () {
+          window.webContents.send('darkmode', systemPreferences.isDarkMode())
+        }
+    );
+  }
 
-
-  ipcMain.on('start', () => {
+  ipcMain.on("start", () => {
     window.setSize(300, 77, true);
     window.setWindowButtonVisibility(false);
     window.setFocusable(false);
   });
 
-  ipcMain.on('end', (event, end = false, params = {}) => {
+  ipcMain.on("end", (event, end = false, params = {}) => {
     window.setSize(300, 202, true);
     window.setWindowButtonVisibility(true);
     window.setFocusable(true);
     if (end) {
       new Notification({
-        title: `Take a ${params.max === params.current ? 'long':'short'} break`,
-        body: `Pomodoro phase ${params.current - 1} done, ${params.max === params.current ? '25':'5'} minutes of free time`,
+        title: `Take a ${
+          params.max === params.current ? "long" : "short"
+        } break`,
+        body: `Pomodoro phase ${params.current - 1} done, ${
+          params.max === params.current ? "25" : "5"
+        } minutes of free time`
       }).show();
     }
     window.focus();
   });
 
-  ipcMain.on('settings-change', (event, data) => {
+  ipcMain.on("settings-change", (event, data) => {
     if (data.alwaysOnTop !== undefined) window.setAlwaysOnTop(data.alwaysOnTop);
   });
 
-  ipcMain.on('action', (event, data) => {
+  ipcMain.on("action", (event, data) => {
     switch (data) {
-      case 'play': {
+      case "play": {
         window.setWindowButtonVisibility(false);
         break;
       }
-      case 'pause': {
+      case "pause": {
         window.setWindowButtonVisibility(true);
         break;
       }
     }
   });
 
-  ipcMain.on('log', (event, data) => {
+  ipcMain.on("log", (event, data) => {
     switch (data.type) {
-      case 'log': {
-        data.message.unshift('\x1b[34m');
+      case "log": {
+        data.message.unshift("\x1b[34m");
         console.log.apply(null, data.message);
         break;
       }
-      case 'error': {
-        data.message.unshift('\x1b[35m');
+      case "error": {
+        data.message.unshift("\x1b[35m");
         console.warn.apply(null, data.message);
         break;
       }
-      case 'warn': {
-        data.message.unshift('\x1b[33m');
+      case "warn": {
+        data.message.unshift("\x1b[33m");
         console.error.apply(null, data.message);
         break;
       }
     }
   });
 
-  app.on('window-all-closed', () => {
+  app.on("window-all-closed", () => {
     app.quit();
-  })
+  });
 });
